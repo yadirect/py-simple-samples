872e061f1a23f47222ed2681990a4d73bde77fc5
yadirect <yadirect@email.com>
2019-02-11 22:06:31 +0300

    Added new files

--- src/calc.py
+++ src/calc.py
@@ -11,5 +11,9 @@ elif op == "/":
      print(num1 / num2)
 elif op == "*":
      print(num1 * num2)
+elif op == "**":
+     print(num1 ** num2)
+elif op == "%":
+     print(num1 % num2)
 else:
      print("Invalid Operator")
